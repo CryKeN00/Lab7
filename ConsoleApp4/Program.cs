8357a2ff8041e0fc4b4ccb8bcf2c6138c87356ce
CryKeN <CryKeN@DESKTOP-39H65C5>
2023-11-20 19:13:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите строку");
+string str = Convert.ToString(Console.ReadLine());
+string[] words = str.Split(new char[] { ' ', ',' }) ;
+int koll = words.Length - 1;
+Console.WriteLine($"Количество слов равно {koll}") ;
